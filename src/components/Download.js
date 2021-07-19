b1bcc98afa44b5bdf64e02b75b69fedce3f64b73
TofuBoy <grandfunk30@gmail.com>
2021-07-18 19:39:14 -0500

    Adding CV PDF

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 export const Download = () => {
+
+   
+
     return (
         <section className="contact section" >
                 <h2 className="section-title">Curriculum</h2>
                          <i id='download' className='download__element bx bxs-file-pdf'></i>
                          
-                        <button type="button"  className="download__element download__button button">Dowload CV</button> 
+                        <a type="button" href="./assets/pedroarcecv.pdf"  download id='dl' className="download__element download__button button" rel="noreferrer">Download CV</a> 
                 
             </section>
     )
